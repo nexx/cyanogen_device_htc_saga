292a8bf48331f0e652100733df2ff1364a8b14ab
Nexx <nexx@nexxdesign.co.uk>
2011-07-28 19:55:03 -0400

    Initial device setup for Saga - WIP

---
+++ saga.mk
@@ -0,0 +1,167 @@
+#
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+
+PRODUCT_COPY_FILES += device/htc/saga/gps.conf:system/etc/gps.conf
+
+## (1) First, the most specific values, i.e. the aspects that are specific to GSM
+
+PRODUCT_COPY_FILES += \
+    device/htc/saga/init.saga.rc:root/init.saga.rc \
+    device/htc/saga/ueventd.saga.rc:root/ueventd.saga.rc
+
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+## (2) Also get non-open-source GSM-specific aspects if available
+$(call inherit-product-if-exists, vendor/htc/saga/saga-vendor.mk)
+
+## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-htc \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
+    ro.com.google.gmsversion=2.3_r3 \
+    ro.setupwizard.enable_bypass=1 \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/saga/overlay
+
+# gsm config xml file
+PRODUCT_COPY_FILES += \
+    device/htc/saga/spn-conf.xml:system/etc/spn-conf.xml \
+    device/htc/saga/voicemail-conf.xml:system/etc/voicemail-conf.xml
+
+PRODUCT_PACKAGES += \
+    lights.saga \
+    sensors.saga \
+    gps.saga
+
+# Keychars
+PRODUCT_COPY_FILES += \
+    device/htc/saga/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
+    device/htc/saga/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
+    device/htc/saga/keychars/saga-keypad.kcm.bin:system/usr/keychars/saga-keypad.kcm.bin \
+    device/htc/saga/keychars/saga-keypad-wwe.kcm.bin:system/usr/keychars/saga-keypad-wwe.kcm.bin
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    device/htc/saga/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/htc/saga/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/saga/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/htc/saga/keylayout/saga-keypad.kl:system/usr/keylayout/saga-keypad.kl \
+    device/htc/saga/keylayout/saga-keypad-wwe.kl:system/usr/keylayout/saga-keypad-wwe.kl
+
+# Firmware
+PRODUCT_COPY_FILES += \
+    device/htc/saga/firmware/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
+    device/htc/saga/firmware/default.acdb:system/etc/firmware/default.acdb \
+    device/htc/saga/firmware/default_org.acdb:system/etc/firmware/default_org.acdb \
+    device/htc/saga/firmware/default_org_wb.acdb:system/etc/firmware/default_org_wb.acdb \
+    device/htc/saga/firmware/mp2_dec_mc.fw:system/etc/firmware/mp2_dec_mc.fw \
+    device/htc/saga/firmware/vidc_720p_command_control.fw:system/etc/firmware/vidc_720p_command_control.fw \
+    device/htc/saga/firmware/vidc_720p_h263_dec_mc.fw:system/etc/firmware/vidc_720p_h263_dec_mc.fw \
+    device/htc/saga/firmware/vidc_720p_h264_dec_mc.fw:system/etc/firmware/vidc_720p_h264_dec_mc.fw \
+    device/htc/saga/firmware/vidc_720p_h264_enc_mc.fw:system/etc/firmware/vidc_720p_h264_enc_mc.fw \
+    device/htc/saga/firmware/vidc_720p_mp2_dec_mc.fw:system/etc/firmware/vidc_720p_mp2_dec_mc.fw \
+    device/htc/saga/firmware/vidc_720p_mp4_dec_mc.fw:system/etc/firmware/vidc_720p_mp4_dec_mc.fw \
+    device/htc/saga/firmware/vidc_720p_mp4_enc_mc.fw:system/etc/firmware/vidc_720p_mp4_enc_mc.fw \
+    device/htc/saga/firmware/vidc_720p_vc1_dec_mc.fw:system/etc/firmware/vidc_720p_vc1_dec_mc.fw \
+    device/htc/saga/firmware/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
+    device/htc/saga/firmware/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw
+
+# Audio DSP Profiles
+PRODUCT_COPY_FILES += \
+    device/htc/saga/dsp/AdieHWCodec.csv:system/etc/AdieHWCodec.csv \
+    device/htc/saga/dsp/AIC3254_REG_DualMic.csv:system/etc/AIC3254_REG_DualMic.csv \
+    device/htc/saga/dsp/AIC3254_REG_DualMic.txt:system/etc/AIC3254_REG_DualMic.txt \
+    device/htc/saga/dsp/AIC3254_REG_DualMic_XB.csv:system/etc/AIC3254_REG_DualMic_XB.csv \
+    device/htc/saga/dsp/AIC3254_REG_DualMic_XC.csv:system/etc/AIC3254_REG_DualMic_XC.csv \
+    device/htc/saga/dsp/AudioBTID.csv:system/etc/AudioBTID.csv \
+    device/htc/saga/dsp/TPA2051_CFG.csv:system/etc/TPA2051_CFG.csv \
+    device/htc/saga/dsp/TPA2051_CFG_NEL.csv:system/etc/TPA2051_CFG_NEL.csv \
+    device/htc/saga/dsp/CodecDSPID.txt:system/etc/CodecDSPID.txt \
+    device/htc/saga/dsp/CodecDSPID_WB.txt:system/etc/CodecDSPID_WB.txt \
+    device/htc/saga/dsp/soundimage/Sound_Bass_Booster.txt:system/etc/soundimage/Sound_Bass_Booster.txt \
+    device/htc/saga/dsp/soundimage/Sound_Blues.txt:system/etc/soundimage/Sound_Blues.txt \
+    device/htc/saga/dsp/soundimage/Sound_Classical.txt:system/etc/soundimage/Sound_Classical.txt \
+    device/htc/saga/dsp/soundimage/Sound_Country.txt:system/etc/soundimage/Sound_Country.txt \
+    device/htc/saga/dsp/soundimage/Sound_Jazz.txt:system/etc/soundimage/Sound_Jazz.txt \
+    device/htc/saga/dsp/soundimage/Sound_Latin.txt:system/etc/soundimage/Sound_Latin.txt \
+    device/htc/saga/dsp/soundimage/Sound_New_Age.txt:system/etc/soundimage/Sound_New_Age.txt \
+    device/htc/saga/dsp/soundimage/Sound_Original.txt:system/etc/soundimage/Sound_Original.txt \
+    device/htc/saga/dsp/soundimage/Sound_Original_SPK.txt:system/etc/soundimage/Sound_Original_SPK.txt \
+    device/htc/saga/dsp/soundimage/Sound_Phone_Original_REC.txt:system/etc/soundimage/Sound_Phone_Original_REC.txt \
+    device/htc/saga/dsp/soundimage/Sound_Phone_Original_REC_WB.txt:system/etc/soundimage/Sound_Phone_Original_REC_WB.txt \
+    device/htc/saga/dsp/soundimage/Sound_Piano.txt:system/etc/soundimage/Sound_Piano.txt \
+    device/htc/saga/dsp/soundimage/Sound_Pop.txt:system/etc/soundimage/Sound_Pop.txt \
+    device/htc/saga/dsp/soundimage/Sound_R_B.txt:system/etc/soundimage/Sound_R_B.txt \
+    device/htc/saga/dsp/soundimage/Sound_Recording.txt:system/etc/soundimage/Sound_Recording.txt \
+    device/htc/saga/dsp/soundimage/Sound_Rock.txt:system/etc/soundimage/Sound_Rock.txt \
+    device/htc/saga/dsp/soundimage/Sound_SRS_A_HP.txt:system/etc/soundimage/Sound_SRS_A_HP.txt \
+    device/htc/saga/dsp/soundimage/Sound_SRS_A_SPK.txt:system/etc/soundimage/Sound_SRS_A_SPK.txt \
+    device/htc/saga/dsp/soundimage/Sound_SRS_V_HP.txt:system/etc/soundimage/Sound_SRS_V_HP.txt \
+    device/htc/saga/dsp/soundimage/Sound_SRS_V_SPK.txt:system/etc/soundimage/Sound_SRS_V_SPK.txt \
+    device/htc/saga/dsp/soundimage/Sound_Treble_Booster.txt:system/etc/soundimage/Sound_Treble_Booster.txt \
+    device/htc/saga/dsp/soundimage/Sound_Vocal_Booster.txt:system/etc/soundimage/Sound_Vocal_Booster.txt
+ 
+PRODUCT_COPY_FILES += \
+    device/htc/saga/vold.fstab:system/etc/vold.fstab
+
+# media config xml file
+PRODUCT_COPY_FILES += \
+    device/htc/saga/media_profiles.xml:system/etc/media_profiles.xml
+
+# Kernel modules
+#PRODUCT_COPY_FILES += \
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/htc/saga/prebuilt/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+PRODUCT_COPY_FILES += \
+    device/htc/saga/prebuilt/bcm4329.ko:system/lib/modules/bcm4329.ko
+
+$(call inherit-product-if-exists, vendor/htc/saga/saga-vendor.mk)
+
+# common msm7x30 configs
+$(call inherit-product, device/htc/msm7x30-common/msm7x30.mk)
+
+# media profiles and capabilities spec
+$(call inherit-product, device/htc/saga/media_a1026.mk)
+
+# htc audio settings
+$(call inherit-product, device/htc/saga/media_htcaudio.mk)
+
+# stuff common to all HTC phones
+$(call inherit-product, device/htc/common/common.mk)
+
+$(call inherit-product, build/target/product/full_base.mk)
+
+PRODUCT_NAME := htc_saga
+PRODUCT_DEVICE := saga
+PRODUCT_MODEL := HTC Desire S
+PRODUCT_MANUFACTURER := HTC
