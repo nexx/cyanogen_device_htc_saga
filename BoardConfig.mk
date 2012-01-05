d64ade9188f50f4c4d14e62bbf4d82b3d920b5de
Simon Davie <nexx@nexxdesign.co.uk>
2012-01-05 15:07:05 +0000

    saga: Enable USB mass storage
    Our current kernel lacks the new USB gadget support, as such we use
    the legacy USB mode along with these changes.
    
    Change-Id: Id0f771d87be0ba0c0cbb241d61284e5c40af32b2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,3 +76,5 @@ BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
