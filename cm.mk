fe1a6b48c72e5b8681da7ef24647c85544b9cd3c
Simon Davie <nexx@nexxdesign.co.uk>
2012-03-08 23:24:28 +0000

    saga: Don't override BUILD_ID
    Change-Id: Iba1eebb3501086a1b18fafb7cded8ab5923b1b91

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_saga BUILD_ID=IML74K BUILD_FINGERPRINT=htc_europe/htc_saga/saga:2.3.5/GRJ90/156318.4:user/release-keys PRIVATE_BUILD_DESC="2.10.401.4 CL156318 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_saga BUILD_FINGERPRINT=htc_europe/htc_saga/saga:2.3.5/GRJ90/156318.4:user/release-keys PRIVATE_BUILD_DESC="2.10.401.4 CL156318 release-keys"
 
 PRODUCT_NAME := cm_saga
 PRODUCT_DEVICE := saga
